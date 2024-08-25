1675deaaa6cd48d08fc5dad4d8b81812a73d018b
Khadija MAKKAOUI <makkaoui.khadija44@gmail.com>
2024-08-25 15:06:44 +0100

    Implement Function-based View Implement Class-based View

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from .models import Book
+from .models import Library
+
 
 # Create your views here.
+def list_books(request):
+    books = Book.objects.all()
+    response_text = '\n'.join([f"{book.title} by {book.author}" for book in books])
+    return HttpResponse(response_text, content_type='text/plain')
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'  # You need to create this template
+    context_object_name = 'library'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['books'] = self.object.books.all()  # Assuming a ForeignKey or ManyToMany relationship
+        return context
