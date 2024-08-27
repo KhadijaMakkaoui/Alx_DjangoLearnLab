f5afc4cc626ea79bf567c0bf3c356e7b730ead74
Khadija Makkaoui <makkaoui.khadija44@gmail.com>
2024-08-27 16:22:25 +0100

    code refactor

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -8,6 +8,6 @@ class Book(models.Model):
     author= models.CharField(max_length=100)
     publication_year = models.IntegerField
 
-class CustomUser(AbstractUser,models.Model):
+class CustomUser(AbstractUser):
     date_of_birth=models.DateField
     profile_photo=models.ImageField
