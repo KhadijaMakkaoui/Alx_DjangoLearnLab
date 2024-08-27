6d090eb06122292ebaa0b363051f51b09eddd48c
Khadija Makkaoui <makkaoui.khadija44@gmail.com>
2024-08-27 15:10:39 +0100

    refactor code

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -9,7 +9,7 @@ urlpatterns = [
     path('login/', views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
     path('logout/',views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
     path('register/', views.register, name='register'),
-     path('admin/', admin_view, name='admin'),
+    path('admin/', admin_view, name='admin'),
     path('librarian/', librarian_view, name='librarian'),
     path('member/', member_view, name='member'),
 ]
