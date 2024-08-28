dcc5bbef95cca04b041d8c73d13dd9f454002482
Khadija Makkaoui <makkaoui.khadija44@gmail.com>
2024-08-28 12:48:50 +0100

    define permission

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics, viewsets
 from .serializers import BookSerializer
 from .models import Book
+from rest_framework.permissions import IsAuthenticated
+
 # Create your views here.
 class BookList(generics.ListAPIView):
     queryset = Book.objects.all()
@@ -10,4 +12,5 @@ class BookList(generics.ListAPIView):
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
+     permission_classes = [IsAuthenticated]  # Only authenticated users can access
 
