098c02d68cb561ab4f3943ec69039be3c8f7288f
Khadija Makkaoui <makkaoui.khadija44@gmail.com>
2024-08-28 09:38:12 +0100

    Secure data access in views

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404
+from .models import Book
+from django import forms
+from .forms import ExampleForm
 # Create your views here.
+def book_detail(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+    return render(request, 'books/book_detail.html', {'book': book})
+
+
+class BookSearchForm(forms.Form):
+    query = forms.CharField(max_length=100)
+
+def search_books(request):
+    form = BookSearchForm(request.GET)
+    if form.is_valid():
+        query = form.cleaned_data['query']
+        books = Book.objects.filter(title__icontains=query)
+        return render(request, 'books/search_results.html', {'books': books})
+    else:
+        return render(request, 'books/search.html', {'form': form})
