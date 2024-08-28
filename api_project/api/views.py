ada769c25210ef1baaedb3713194608781301ff9
Khadija Makkaoui <makkaoui.khadija44@gmail.com>
2024-08-28 12:32:43 +0100

    Create viewset

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,6 +4,7 @@ from .serializers import BookSerializer
 from .models import Book
 # Create your views here.
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
     serializer_class = BookSerializer
 
 class BookViewSet(viewsets.ModelViewSet):
