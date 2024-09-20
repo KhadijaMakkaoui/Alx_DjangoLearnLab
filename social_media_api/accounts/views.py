8e739e85727dcb07f5153f30f6d5270423806479
Khadija Makkaoui <makkaoui.khadija44@gmail.com>
2024-09-20 15:58:33 +0100

    Create API Endpoints for Managing Follows

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,8 +1,11 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, status, permissions
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from .serializers import UserRegistrationSerializer, UserLoginSerializer, UserProfileSerializer
 from django.contrib.auth import get_user_model
+from rest_framework.decorators import api_view, permission_classes
+from django.shortcuts import get_object_or_404
+from .models import CustomUser
 
 User = get_user_model()
 
@@ -26,3 +29,21 @@ class UserProfileView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+
+@api_view(['POST'])
+@permission_classes([permissions.IsAuthenticated])
+def follow_user(request, user_id):
+    user_to_follow = get_object_or_404(CustomUser, id=user_id)
+    if request.user == user_to_follow:
+        return Response({'error': "You can't follow yourself"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    request.user.following.add(user_to_follow)
+    return Response({'success': f'You are now following {user_to_follow.username}'}, status=status.HTTP_200_OK)
+
+@api_view(['POST'])
+@permission_classes([permissions.IsAuthenticated])
+def unfollow_user(request, user_id):
+    user_to_unfollow = get_object_or_404(CustomUser, id=user_id)
+    request.user.following.remove(user_to_unfollow)
+    return Response({'success': f'You have unfollowed {user_to_unfollow.username}'}, status=status.HTTP_200_OK)
