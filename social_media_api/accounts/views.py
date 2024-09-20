98c3fb4bf228ad5ed82ef72b7160bed9d9e757a4
Khadija Makkaoui <makkaoui.khadija44@gmail.com>
2024-09-20 10:27:29 +0100

    Create Views for CRUD Operations

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -1,8 +1,10 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions,viewsets
 from rest_framework.response import Response
 from rest_framework.authtoken.models import Token
 from .serializers import UserRegistrationSerializer, UserLoginSerializer, UserProfileSerializer
 from django.contrib.auth import get_user_model
+from .models import Post, Comment
+from .serializers import PostSerializer, CommentSerializer
 
 User = get_user_model()
 
@@ -26,3 +28,19 @@ class UserProfileView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all().order_by('-created_at')
+    serializer_class = PostSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
